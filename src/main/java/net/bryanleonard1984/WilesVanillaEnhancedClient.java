5565fe3c636df412eec317525840d55ebd89cbd8
bryanleonard1984 <bryan.leonard75@gmail.com>
2025-01-20 14:02:05 -0700

    Initial commit

---
+++ src/main/java/net/bryanleonard1984/WilesVanillaEnhancedClient.java
@@ -0,0 +1,12 @@
+package net.bryanleonard1984;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WilesVanillaEnhancedClient implements ClientModInitializer
+{
+    @Override
+    public void onInitializeClient()
+    {
+        //TODO
+    }
+}
